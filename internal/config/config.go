a517fe25debe6ab4c0daf1ea08b8a7dca29fa107
Dominic Breuker <dominic.breuker@gmail.com>
2018-02-20 19:33:03 +0100

    start big refactoring

---
+++ internal/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import "fmt"
+
+type Config struct {
+	RDirs []string
+	Dirs  []string
+	LogFS bool
+	LogPS bool
+}
+
+func (c Config) String() string {
+	return fmt.Sprintf("Printing events: processes: %t | file system events: %t | Watching directories: %+v (recursive) | %+v (non-recursive)", c.LogPS, c.LogFS, c.RDirs, c.Dirs)
+}
