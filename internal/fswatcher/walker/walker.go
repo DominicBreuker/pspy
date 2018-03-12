723fb79e0671adc647390bdf633b3d3fcf792aba
Dominic Breuker <dominic.breuker@gmail.com>
2018-03-12 08:40:30 +0100

    refactor walker

--- internal/fswatcher/walker/walker.go
+++ internal/fswatcher/walker/walker.go
@@ -7,10 +7,25 @@ import (
 	"path/filepath"
 )
 
-type Walker struct{}
+type Walker struct {
+}
 
 func NewWalker() *Walker {
 	return &Walker{}
+}
+
+type chans struct {
+	dirCh  chan string
+	errCh  chan error
+	doneCh chan struct{}
+}
+
+func newChans() *chans {
+	return &chans{
+		dirCh:  make(chan string),
+		errCh:  make(chan error),
+		doneCh: make(chan struct{}),
+	}
 }
 
 const maxInt = int(^uint(0) >> 1)
@@ -19,41 +34,51 @@ func (w *Walker) Walk(root string, depth int) (dirCh chan string, errCh chan err
 	if depth < 0 {
 		depth = maxInt
 	}
-	dirCh = make(chan string)
-	errCh = make(chan error)
-	doneCh = make(chan struct{})
+	c := newChans()
 
 	go func() {
 		defer close(dirCh)
-		descent(root, depth-1, dirCh, errCh, doneCh)
+		descent(root, depth-1, c)
 	}()
-	return dirCh, errCh, doneCh
+	return c.dirCh, c.errCh, c.doneCh
 }
 
-func descent(dir string, depth int, dirCh chan string, errCh chan error, doneCh chan struct{}) {
-	_, err := os.Stat(dir)
-	if err != nil {
-		errCh <- fmt.Errorf("visiting %s: %v", dir, err)
-		return
-	}
-	select {
-	case dirCh <- dir:
-	case <-doneCh:
-		return
-	}
-	if depth < 0 {
+func descent(dir string, depth int, c *chans) {
+	if done := emitDir(dir, depth, c); done {
 		return
 	}
 
+	handleSubDirs(dir, depth, c)
+}
+
+func emitDir(dir string, depth int, c *chans) bool {
+	_, err := os.Stat(dir)
+	if err != nil {
+		c.errCh <- fmt.Errorf("visiting %s: %v", dir, err)
+		return true
+	}
+	select {
+	case c.dirCh <- dir:
+	case <-c.doneCh:
+		return true
+	}
+	if depth < 0 {
+		return true
+	}
+
+	return false
+}
+
+func handleSubDirs(dir string, depth int, c *chans) {
 	ls, err := ioutil.ReadDir(dir)
 	if err != nil {
-		errCh <- fmt.Errorf("opening dir %s: %v", dir, err)
+		c.errCh <- fmt.Errorf("opening dir %s: %v", dir, err)
 	}
 
 	for _, e := range ls {
 		if e.IsDir() {
 			newDir := filepath.Join(dir, e.Name())
-			descent(newDir, depth-1, dirCh, errCh, doneCh)
+			descent(newDir, depth-1, c)
 		}
 	}
 }
